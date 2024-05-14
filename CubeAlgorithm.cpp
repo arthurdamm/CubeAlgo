db59ae46081673fbdf32ec8ca3d99ef3f4f7caf3
Arthur Damm <indigoarthur@gmail.com>
2024-05-13 18:16:55 -0700

    Initial commit

---
+++ CubeAlgorithm.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CubeAlgorithm.h"
+
+CubeAlgorithm::CubeAlgorithm()
+{
+    UE_LOG(LogTemp, Error, TEXT("CubeAlgorithm()"));
+    test();
+}
+
+CubeAlgorithm::~CubeAlgorithm()
+{
+}
+
+void CubeAlgorithm::test() {
+    UE_LOG(LogTemp, Error, TEXT("CubeAlgorithm::test()"));
+}
