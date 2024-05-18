9a94be4e412df5d057c05cd3e4ca1b9101968f70
Arthur Damm <indigoarthur@gmail.com>
2024-05-18 16:49:07 -0700

    CubeAlgo class w/ rotations

--- CubeAlgorithm.cpp
+++ CubeAlgorithm.cpp
@@ -3,58 +3,28 @@
 
 #include "CubeAlgorithm.h"
 
+int dtoi(double n) {
+    int result = static_cast<int64>(fabs(round(n)));
+    // UE_LOG(LogTemp, Display, TEXT("dtoi(%f)=%d"), n, result);
+    return result;
+}
+
 CubeAlgorithm::CubeAlgorithm()
 {
     UE_LOG(LogTemp, Error, TEXT("CubeAlgorithm()"));
+    UE_LOG(LogTemp, Warning, TEXT("sizes: %d %d %d"), sizeof(float), sizeof(double), sizeof(long double));
+    populateRotators();
+    FVector test = FVector(0, 1, 1);
+    test = layerToRotator[0].TransformVector(test);
+    UE_LOG(LogTemp, Error, TEXT("TEST: %s"), *test.ToString());
     init();
-    test();
+    print();
+    rotateLayer(0, 1);
     print();
 }
 
 CubeAlgorithm::~CubeAlgorithm()
 {
-}
-
-void PerformRotations()
-{
-    FVector VecX(1, 0, 0);  // Vector along the X-axis
-    FVector VecZ(0, 0, 1);  // Vector along the Z-axis
-
-    // Roll around the Y-axis
-    FRotator RollRotator(0, 0, 90);  // Pitch, Yaw, Roll
-    FMatrix RollMatrix = FRotationMatrix(RollRotator);
-    FVector RotatedVecX = RollMatrix.TransformVector(VecX);
-    FVector RotatedVecZ = RollMatrix.TransformVector(VecZ);
-
-    UE_LOG(LogTemp, Warning, TEXT("Original Vector X: %s, Rotated Vector X by Roll: %s"), *VecX.ToString(), *RotatedVecX.ToString());
-    UE_LOG(LogTemp, Warning, TEXT("Original Vector Z: %s, Rotated Vector Z by Roll: %s"), *VecZ.ToString(), *RotatedVecZ.ToString());
-}
-
-void CubeAlgorithm::test() {
-    UE_LOG(LogTemp, Error, TEXT("CubeAlgorithm::test()"));
-
-    FVector vector = FVector(1, 0, 0);
-    std::vector<FVector> vecs = {FVector(1, -1, 1), FVector(-1, -1, -1), FVector(0, 0, 1), FVector(1, 0, 1), FVector(0, 1, 0), FVector(0, 1, -1)};
-    std::vector<FRotator> rots = {FRotator(-90, 0, 0), FRotator(0, -90, 0), FRotator(0, 0, -90)};
-    FRotator rotator = FRotator(0, 90, 0);
-    FMatrix rotationMatrix = FRotationMatrix(rotator);
-    FVector rotatedVector = rotationMatrix.TransformVector(vector);
-    UE_LOG(LogTemp, Error, TEXT("vector: %s rotatedVector: %s"), *vector.ToString(), *rotatedVector.ToString());
-
-    UE_LOG(LogTemp, Error, TEXT("vecs.size(): %d"), vecs.size());
-    for (int i = 0; i < vecs.size(); i++) {
-        vector = vecs[i];
-        UE_LOG(LogTemp, Warning, TEXT("for vector: %s"), *vector.ToString());
-        for (int j = 0; j < rots.size(); j++) {
-            rotator = rots[j];
-            rotationMatrix = FRotationMatrix(rotator);
-            rotatedVector = rotationMatrix.TransformVector(vector);
-            // UE_LOG(LogTemp, Warning, TEXT("rotationMatrix: %s"), *rotationMatrix.ToString());
-            UE_LOG(LogTemp, Error, TEXT("rotator: %s rotatedVector: %s"), *rotator.ToString(), *rotatedVector.ToString());
-        }
-        
-    }
-    // PerformRotations();
 }
 
 void CubeAlgorithm::print() {
@@ -64,8 +34,7 @@ void CubeAlgorithm::print() {
         for (size_t y = 0; y < N; y++) {
             row = "";
             for (size_t x = 0; x < N; x++) {
-                row += cubes[x][y][z].ToString() + " ";
-
+                row += cubes[x][y][z].ToString();
             }
             UE_LOG(LogTemp, Error, TEXT("%s"), *row);
         }
@@ -76,8 +45,75 @@ void CubeAlgorithm::init() {
     for (size_t z = 0; z < N; z++) {
         for (size_t y = 0; y < N; y++) {    
             for (size_t x = 0; x < N; x++) {
-                cubes[x][y][z] = FVector(1, 0, 0);
+                cubes[x][y][z].indices[0] = x;
+                cubes[x][y][z].indices[1] = y;
+                cubes[x][y][z].indices[2] = z;
+                cubes[x][y][z].location = FVector(x, y, z); 
             }
         }
+    }
+}
+
+void CubeAlgorithm::populateRotators() {
+    for (int layer = 0; layer < MAX_LAYERS; layer++) {
+        if (layer >= 0 && layer < N) {
+            layerToRotator[layer] = FRotationMatrix(FRotator(0, 0, 90));
+        } else if (layer >= N && layer < 2 * N) {
+            layerToRotator[layer] = FRotationMatrix(FRotator(90, 0, 0));
+        } else if (layer >= 2 * N && layer < 3 * N) {
+            layerToRotator[layer] = FRotationMatrix(FRotator(0, 90, 0));
+        }
+    }
+}
+
+void CubeAlgorithm::rotateLayer(int layer, int direction) {
+    std::vector<Cube> layerCubes = getLayer(layer);
+    FVector indices;
+    for (auto cube : layerCubes) {
+        indices.X = cube.indices[0] - 1;
+        indices.Y = cube.indices[1] - 1;
+        indices.Z = cube.indices[2] - 1;
+        UE_LOG(LogTemp, Warning, TEXT("cube before: %s"), *cube.ToString());
+        // UE_LOG(LogTemp, Warning, TEXT("indices before: %s"), *indices.ToString());
+        cube.facing = layerToRotator[layer].TransformVector(cube.facing);
+        indices = layerToRotator[layer].TransformVector(indices);
+        // UE_LOG(LogTemp, Warning, TEXT("%s"), *cube.ToString());
+        // UE_LOG(LogTemp, Error, TEXT("indices after: %s"), *indices.ToString());
+        cube.indices[0] = dtoi(indices.X + 1);
+        cube.indices[1] = dtoi(indices.Y + 1);
+        cube.indices[2] = dtoi(indices.Z + 1);
+        UE_LOG(LogTemp, Error, TEXT("cube after: %s"), *cube.ToString());
+        cubes[cube.indices[0]][cube.indices[1]][cube.indices[2]] = cube;
+        UE_LOG(LogTemp, Display, TEXT("."));
+    }
+}
+
+std::vector<Cube> CubeAlgorithm::getLayer(int layer) {
+    std::vector<Cube> layerCubes;
+    for (size_t a = 0; a < N; a++) {
+        for (size_t b = 0; b < N; b++) {
+            size_t x = 0, y = 0, z = 0;
+            adjustCoordinatesToLayer(layer, a, b, x, y, z);
+            layerCubes.push_back(cubes[x][y][z]);
+            // UE_LOG(LogTemp, Display, TEXT("Before: layer:%d x:%d y:%d z:%d"), layer, x, y, z);
+
+        }
+    }
+    return layerCubes;
+}
+
+void CubeAlgorithm::adjustCoordinatesToLayer(size_t layer, size_t a, size_t b, size_t &x, size_t &y, size_t &z) {
+    if (layer >= 0 && layer <= 2) {
+        x = layer - 0;
+        y = a;
+        z = b;
+    } else if (layer >= 3 && layer <= 5) {
+        x = a;
+        y = layer - 3;
+        z = b;
+    } else if (layer >= 6 && layer <= 8) {
+        x = a;
+        y = b;
+        z = layer - 6;
     }
 }
--- CubeAlgorithm.h
+++ CubeAlgorithm.h
@@ -5,18 +5,43 @@
 #include "CoreMinimal.h"
 
 #define N 3
+#define MAX_LAYERS N*N
+
+struct Cube {
+	FVector location;
+	FVector facing;
+	int64 indices[3];
+	Cube(): location(0, 0, 0), facing(1, 0, 0), indices{0} {}
+	FString ToString() {
+		return "<L:(" + location.ToString() + ") F:(" + facing.ToString()
+			+ ") [" + FString::FromInt(indices[0]) + "][" + FString::FromInt(indices[1])
+			+ "][" + FString::FromInt(indices[2]) + "]>, ";
+	}
+};
+
 /**
  * 
  */
 class CUBEALGO_API CubeAlgorithm
 {
 private:
-	FVector cubes[N][N][N];
+	// 3D N^3 sized grid containing forward vectors at each (x,y,z)
+	// position (small cube) in the large rubik's cube
+	Cube cubes[N][N][N];
+
+	FMatrix layerToRotator[MAX_LAYERS];
+
+	void adjustCoordinatesToLayer(size_t layer, size_t a, size_t b, size_t &x, size_t &y, size_t &z);
+	void populateRotators();
+	std::vector<Cube> getLayer(int layer);
+
+	
 public:
 	CubeAlgorithm();
 	~CubeAlgorithm();
-	void test();
 	void print();
 	void init();
+	// rotates given layer 90 degrees in positive or negative direction
+	void rotateLayer(int layer, int direction);
 
 };
