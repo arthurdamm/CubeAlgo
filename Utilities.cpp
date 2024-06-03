bbbe2d785b2f1f29d92469d6e25cf4a83eb35fbf
Arthur Damm <indigoarthur@gmail.com>
2024-06-02 21:23:14 -0700

    Helper funcs for quat and negative zero comparison

--- Utilities.cpp
+++ Utilities.cpp
@@ -1,3 +1,15 @@
 int dtoi(double n) {
     return static_cast<int>(fabs(round(n)));
 }
+
+double HandleNegativeZero(double value) {
+    if (value == -0.0) {
+        return 0.0;
+    }
+    return value;
+}
+
+bool AreQuatsEqual(const FQuat& A, const FQuat& B)
+{
+    return (A.Equals(B, 1e-6f) || A.Equals(-B, 1e-6f));
+}
